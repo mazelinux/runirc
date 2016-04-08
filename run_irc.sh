af9d048cd61c79ed4c29c0f793efa70c17124993
Maze ma <maze.ma1251228@gmail.com>
2016-04-08 23:08:16 +0800

    add files

---
+++ run_irc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#This script is to run irssi auto 
+SERVICE=irc.freenode.net
+PORT=6667
+NICKENAME=mazelinux
+PASSWORD=406732060chen
+
+
+irssic -c $SERVICE -p $PORT -n $NICKNAME -p $PASSWORD
+
